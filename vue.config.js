e9425e6ec90854bd553f67090772d2927c94cfe6
Martin <tincho1597lopez@gmail.com>
2023-03-19 15:29:39 -0300

    Actualizar archivo de configuración de VueJS

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Vue-Movie-App/'
+    : '/',
+  outputDir: 'docs',
+  transpileDependencies: [
+    'vuetify'
+  ],
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Vue App';
+      return args;
+    });
+  }
+}
